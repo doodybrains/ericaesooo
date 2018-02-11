a27d91174bff222173f42fb8b0f4a48ff339aead
doodybrains <doodybrainz@gmail.com>
2018-02-11 16:38:22 -0500

    press

--- src/components/post-item.js
+++ src/components/post-item.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const PostItem = (props) => (
-  <div>
+  <div className="post-item">
     <h2>{props.data.fields.title}</h2>
     <h3>{props.data.fields.text}</h3>
+    <a href={props.data.fields.link} target="_blank">read more</a>
   </div>
 )
 
